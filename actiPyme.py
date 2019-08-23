f0eb46650505623ecfff4d7d269ad15f3cbe2fba
Lavatelli <alberto.lavatelli_altran@dana.com>
2019-08-23 16:03:26 +0200

    Added batch file interface and fixed minor bugs

--- actiPyme.py
+++ actiPyme.py
@@ -408,7 +408,7 @@ class Driver(object):
                     MonthTo, end date want to query timesheet data
                     DayTo, end date want to query timesheet data
 
-            Desc: This function lets you query the data from actitime time sheet. The output is a JSON object
+            Desc: This function lets you query the leave time from actitime time sheet. The output is a JSON object
 
         """
 
@@ -424,3 +424,156 @@ class Driver(object):
             raise Exception('Communication not initialized. Run the Start() command on the driver instance')
 
         return LeaveData
+
+    def GetTimeSheetDateInterval(self,  UserId: int,YearFrom: int,MonthFrom: int,DayFrom: int,YearTo: int,MonthTo: int, DayTo: int):
+        """ 
+            Name: actiPyme.Driver.GetTimeSheetDateInterval(self, UserId: int,YearFrom: int,MonthFrom: int,DayFrom: int,YearTo: int,MonthTo: int, DayTo: int)
+
+            Args:   userId, user number for which you want to read the time sheet
+                    YearFrom, start date you want to query timesheet data
+                    MonthFrom, start date want to query timesheet data
+                    DayFrom, start date want to query timesheet data
+                    YearTo, end date want to query timesheet data
+                    MonthTo, end date want to query timesheet data
+                    DayTo, end date want to query timesheet data
+
+            Desc: This function lets you query the time sheet of a user within a date interval. The output is a JSON object
+
+        """
+        if  self.IsStarted: 
+            assembler="-"
+            dateFrom=assembler.join([str(YearFrom),str(MonthFrom).zfill(2),str(DayFrom).zfill(2)])
+            dateTo=assembler.join([str(YearTo),str(MonthTo).zfill(2),str(DayTo).zfill(2)])
+            requrl=self.Target+"/timetrack?userIds="+str(self.IdNumber)+"&dateFrom="+dateFrom+"&dateTo="+dateTo
+            TimesheetReq=requests.get(requrl,headers=self.ServerHeaders,auth=(self.actitimeUserName,self.actitimePsw))
+            TimesheetData=json.loads(TimesheetReq.text)   
+        else:
+            TimesheetData={}
+            raise Exception('Communication not initialized. Run the Start() command on the driver instance')
+
+        return TimesheetData
+
+
+class AbfParser(object):
+    """ 
+    Name: actiPyme.AbfParser(FileName)
+
+    Args:   FileName, actitime batch format (.abf) file to be read
+
+    Desc:   This class implements the routines to load actitime time track data using a batch file.
+            The batch file should be formatted as described below (comments are with % at the start of line)
+
+                % actitime batch format file (.abf)
+                % comments are with the percentage charachter
+                % A. Lavatelli - 2019
+                %First entry
+                START ENTRY;
+                USERNAME;john.doe;
+                PASSWORD;FooBar18;
+                START TIMESHEET;
+                % format the timesheet as follows: year;month;day;task;hours;
+                2019;8;19;558;1;
+                2019;8;19;13619;7;
+                END TIMESHEET;
+                END ENTRY;
+                %Here another entry
+                START ENTRY;
+                USERNAME;pippo.balera;
+                PASSWORD;ggghhhh;
+                START TIMESHEET;
+                2019;7;12;108;1;
+                2019;7;13;16509;7;
+                2019;8;1;21;4;
+                2019;8;3;009;3;
+                END TIMESHEET;
+                END ENTRY;
+    
+    """
+    def __init__ (self,FileName):
+        self.FileName=FileName
+        # try to open file
+        try:
+            self.InputFile=open(self.FileName,"r")
+        except:
+            raise Exception('Impossible to open file')
+
+        self.TimeSheetRowLimit=1000
+    
+    def Read(self):
+        # remove comment lines
+        ParsedRow=[]
+        for InpLine in self.InputFile:
+            if InpLine[0]!="%":
+                # remove \n
+                JustInp=InpLine.split("\n")[0]
+                ParsedRow.append(JustInp)
+
+        #initialize the data structure
+        timeSheetTemplate={'year': 0,'month':0,'day': 0,'task':0,'hours':0}
+        actiItem={'user':'','psw':'','timesheet': []}
+        actiData=[]
+        timeSheetList=[]
+        Nrows=len(ParsedRow)
+        WorkRow=0
+        EntryCounter=0
+        #here you parse the input file
+        while WorkRow<Nrows:
+            Command=ParsedRow[WorkRow].split(';')
+            if Command[0]=="START ENTRY":
+                actiData.append(actiItem)
+                # get username
+                WorkRow+=1
+                Command=ParsedRow[WorkRow].split(';')
+                if Command[0]=="USERNAME":
+                    actiItem['user']=Command[1]
+                else:
+                    raise ValueError("Input file formatted in the wrong manner. Expected USERNAME after START ENTRY")
+                #get psw
+                WorkRow+=1
+                Command=ParsedRow[WorkRow].split(';')
+                if Command[0]=="PASSWORD":
+                    actiItem['psw']=Command[1]
+                else:
+                    raise ValueError("Input file formatted in the wrong manner. Expected PASSWORD after USERNAME")
+                # retrieve time sheet
+                WorkRow+=1
+                Command=ParsedRow[WorkRow].split(';')
+                if Command[0]=="START TIMESHEET":
+                    TimeSheetRowCounter=0
+                    while Command[0]!="END TIMESHEET":
+                        WorkRow+=1
+                        
+                        # put a limit to the while cycle
+                        if TimeSheetRowCounter>self.TimeSheetRowLimit:
+                            raise  ValueError("Too many time sheet rows or maybe something wrong in the formatting of input file. Check the existance of the END TIMESHEET tag")
+                        Command=ParsedRow[WorkRow].split(';')
+                        # dump the time sheet data into the output array
+                        if Command[0]!="END TIMESHEET":
+                            timeSheetList.append(timeSheetTemplate)
+                            timeSheetTemplate["year"]=int(Command[0])
+                            timeSheetTemplate["month"]=int(Command[1])
+                            timeSheetTemplate["day"]=int(Command[2])
+                            timeSheetTemplate["task"]=int(Command[3])
+                            timeSheetTemplate["hours"]=int(Command[4])
+                            TimeSheetLineAsJsonStr=json.dumps(timeSheetTemplate)
+                            timeSheetList[TimeSheetRowCounter]=json.loads(TimeSheetLineAsJsonStr)
+                            TimeSheetRowCounter+=1
+                    
+                    actiItem["timesheet"]=timeSheetList                  
+                else:
+                    raise ValueError("Input file formatted in the wrong manner. Expected START TIMESHEET after PASSWORD")
+                #update counters
+                WorkRow+=1
+                Command=ParsedRow[WorkRow].split(';')
+                if Command[0]=="END ENTRY":
+                    # dump the acti time entry in the output structure (JSON format)
+                    actiDataAsJsonStr=json.dumps(actiItem)
+                    actiData[EntryCounter]=json.loads(actiDataAsJsonStr)
+                    EntryCounter+=1
+
+            WorkRow+=1
+
+        return(actiData)
+
+    def Close(self):
+        self.InputFile.close()
